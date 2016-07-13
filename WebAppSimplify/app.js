e56cc5a7f827f6739cc323e91d768a27a067e09a
McCormack <e061844@corp.mastercard.org>
2016-07-13 17:56:07 +0100

    Fixing payments for html stuff

--- WebAppSimplify/app.js
+++ WebAppSimplify/app.js
@@ -10,17 +10,28 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var Simplify = require("simplify-commerce"),
+    client = Simplify.getClient({
+        publicKey: 'sbpb_Nzc0MGUyYjMtOWQ4Ni00MThkLTgxZWItNDdjOWFmZmZjN2E3',
+        privateKey: 'KoNnNKCY7uejreCZ9ca6uvyZrC37UMlZFwmwTKkgEO15YFFQL0ODSXAOkNtXTToq'
+    });
+
+// app.js
+//app.engine('html', require('ejs').renderFile);
+//app.set('view engine', 'html');
+//app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3000, function(){
+  console.log('Im starting')
+});
 
 app.use('/', routes);
 app.use('/users', users);
@@ -28,13 +39,7 @@ app.use('/users', users);
 app.post("/payment", function (req, res) 
 {
     console.log('Returned from payment page');
-    console.log(req.body.simplifyToken)
-  
-    var Simplify = require("simplify-commerce"),
-    client = Simplify.getClient({
-        publicKey: 'sbpb_YWI3NWQ4MzgtODhmMC00NzgyLTgzNzItYzY0NzZmNjVjYTNl',
-        privateKey: 'PFao6g1E3h+OyReXU9bY0oJXss8oPvHLWWUCW/aNCSB5YFFQL0ODSXAOkNtXTToq'
-    });
+    console.log(req.body.simplifyToken);
  
     client.payment.create({
       amount : "1000",
@@ -48,41 +53,12 @@ app.post("/payment", function (req, res)
     if(errData){
         console.error("Error Message: " + errData.data.error.message);
         // handle the error
+        res.redirect('/paymentpage.html')
         return;
     }
  
     console.log("Payment Status: " + data.paymentStatus);
-  });
-});
-
-
-app.post("/checkoutBirthday/payment", function (req, res) 
-{
-    console.log('Returned from payment page');
-    console.log(req.body.simplifyToken)
-  
-    var Simplify = require("simplify-commerce"),
-    client = Simplify.getClient({
-        publicKey: 'sbpb_YWI3NWQ4MzgtODhmMC00NzgyLTgzNzItYzY0NzZmNjVjYTNl',
-        privateKey: 'PFao6g1E3h+OyReXU9bY0oJXss8oPvHLWWUCW/aNCSB5YFFQL0ODSXAOkNtXTToq'
-    });
- 
-    client.payment.create({
-      amount : "1000",
-      token : req.body.simplifyToken,
-      description : "payment description",
-      reference : "7a6ef6be31",
-      currency : "USD"
-    },
-    
-    function(errData, data){
-    if(errData){
-        console.error("Error Message: " + errData.data.error.message);
-        // handle the error
-        return;
-    }
- 
-    console.log("Payment Status: " + data.paymentStatus);
+    res.redirect('/index.html')
   });
 });
 
